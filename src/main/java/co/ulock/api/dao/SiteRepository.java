e02589978c01c31dd1c31158d4ab4b481cbdd040
Xavier Jodoin <xavier@jodoin.me>
2016-02-03 11:18:26 -0500

    add transactional annotation on site controller

--- src/main/java/co/ulock/api/dao/SiteRepository.java
+++ src/main/java/co/ulock/api/dao/SiteRepository.java
@@ -3,9 +3,11 @@ package co.ulock.api.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import co.ulock.api.data.Site;
 
+@Transactional
 public interface SiteRepository extends JpaRepository<Site, String> {
 	List<Site> findByAccountId(String accountId);
 }
