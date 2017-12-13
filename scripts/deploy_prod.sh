8aaa92d37034febb084b7e35594d8953345567ec
Xavier Jodoin <xavier@jodoin.me>
2017-12-13 08:45:55 -0500

    fix deploy prod

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -2,7 +2,7 @@
 set -ev
 
 mvn docker:build
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker push ulock/ulock-server
 
 #ssh -tt $REMOTE_USERNAME@$REMOTE_HOST << EOF
